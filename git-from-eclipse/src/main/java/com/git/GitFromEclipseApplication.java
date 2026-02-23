adc5d4d06593ebfb3a4ff309f1e716ad8a4998a3
nagarjunaprasad98 <155219442+nagarjunaprasad98@users.noreply.github.com>
2026-02-23 15:22:48 +0530

    Add print statement in main method
    Added a print statement in the main method.

--- git-from-eclipse/src/main/java/com/git/GitFromEclipseApplication.java
+++ git-from-eclipse/src/main/java/com/git/GitFromEclipseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitFromEclipseApplication {
 
 	public static void main(String[] args) {
+		System.out.print;
 		SpringApplication.run(GitFromEclipseApplication.class, args);
 	}
 
