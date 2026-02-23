d9bd51636aaf69893ed12971d458d2f763df5ef1
nagarjunaprasad98 <nagarjunaprasad99@gmail.com>
2026-02-23 15:28:25 +0530

    just commit

--- git-from-eclipse/src/main/java/com/git/GitFromEclipseApplication.java
+++ git-from-eclipse/src/main/java/com/git/GitFromEclipseApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitFromEclipseApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hi nagarjuna");
+		System.out.println("Hi nagarjuna.....!");
 		SpringApplication.run(GitFromEclipseApplication.class, args);
 	}
 
